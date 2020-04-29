087e96fc547a9f64d10d5f2fe1a8b72e18746f55
HakanBergman <hakan.bergman@drumedar.se>
2020-04-29 22:10:58 +0200

    Update beforeinstall.js
    Changed cloudlets and some other settings

--- scripts/beforeinstall.js
+++ scripts/beforeinstall.js
@@ -5,11 +5,11 @@ var resp = {
   ssl: !!jelastic.billing.account.GetQuotas('environment.jelasticssl.enabled').array[0].value,
   nodes: [{
     nodeType: "storage",
-    flexibleCloudlets: ${settings.st_flexibleCloudlets:8},
+    flexibleCloudlets: ${settings.st_flexibleCloudlets:1},
     fixedCloudlets: ${settings.st_fixedCloudlets:1},
-    diskLimit: ${settings.st_diskLimit:100},
+    diskLimit: ${settings.st_diskLimit:5},
     nodeGroup: "storage",
-    displayName: "Storage"
+    displayName: "DHC Storage"
   }]
 }
 
@@ -17,12 +17,12 @@ if (${settings.galera:false}) {
   resp.nodes.push({
     nodeType: "mariadb-dockerized",
     tag: "10.3.20",
-    count: 3,
-    flexibleCloudlets: ${settings.db_flexibleCloudlets:8},
+    count: 1,
+    flexibleCloudlets: ${settings.db_flexibleCloudlets:1},
     fixedCloudlets: ${settings.db_fixedCloudlets:1},
-    diskLimit: ${settings.db_diskLimit:10},
+    diskLimit: ${settings.db_diskLimit:5},
     nodeGroup: "sqldb",
-    displayName: "Galera cluster",
+    displayName: "DHC MariaDB Cluster",
     restartDelay: 5,
     skipNodeEmails: true,
     env: {
@@ -36,13 +36,13 @@ if (!${settings.galera:false}) {
   resp.nodes.push({
     nodeType: "mariadb-dockerized",
     tag: "10.3.20",
-    count: 2,
-    flexibleCloudlets: ${settings.db_flexibleCloudlets:8},
+    count: 1,
+    flexibleCloudlets: ${settings.db_flexibleCloudlets:1},
     fixedCloudlets: ${settings.db_fixedCloudlets:1},
-    diskLimit: ${settings.db_diskLimit:10},
+    diskLimit: ${settings.db_diskLimit:5},
     nodeGroup: "sqldb",
     skipNodeEmails: true,
-    displayName: "DB Server"
+    displayName: "MariaDB Server"
   })
 }
 
@@ -51,37 +51,37 @@ if (${settings.ls-addon:false}) {
     nodeType: "litespeedadc",
     tag: "2.5.1",
     count: 1,
-    flexibleCloudlets: ${settings.bl_flexibleCloudlets:8},
+    flexibleCloudlets: ${settings.bl_flexibleCloudlets:1},
     fixedCloudlets: ${settings.bl_fixedCloudlets:1},
-    diskLimit: ${settings.bl_diskLimit:10},
+    diskLimit: ${settings.bl_diskLimit:5},
     nodeGroup: "bl",
     scalingMode: "STATEFUL",
-    displayName: "Load balancer",
+    displayName: "DHC Load Balancer",
     env: {
       WP_PROTECT: wpbfp,
-      WP_PROTECT_LIMIT: 100
+      WP_PROTECT_LIMIT: 10
     }
   }, {
     nodeType: "litespeedphp",
-    tag: "5.4.1-php-7.3.7",
-    count: ${settings.cp_count:2},
-    flexibleCloudlets: ${settings.cp_flexibleCloudlets:16},
+    tag: "5.4.6-php-7.4.3",
+    count: ${settings.cp_count:1},
+    flexibleCloudlets: ${settings.cp_flexibleCloudlets:1},
     fixedCloudlets: ${settings.cp_fixedCloudlets:1},
-    diskLimit: ${settings.cp_diskLimit:10},
+    diskLimit: ${settings.cp_diskLimit:5},
     nodeGroup: "cp",
     scalingMode: "STATELESS",
-    displayName: "AppServer",
+    displayName: "DHC WordPress",
     env: {
-      SERVER_WEBROOT: "/var/www/webroot/ROOT",
+      SERVER_WEBROOT: "/mnt/www",
       REDIS_ENABLED: "true",
       WAF: "${settings.waf:false}",
       WP_PROTECT: "OFF"
     },
     volumes: [
-      "/var/www/webroot/ROOT"
+      "/mnt/www/default"
     ],  
     volumeMounts: {
-      "/var/www/webroot/ROOT": {
+      "/mnt/www/default": {
         readOnly: "false",
         sourcePath: "/data/ROOT",
         sourceNodeGroup: "storage"
@@ -95,38 +95,38 @@ if (!${settings.ls-addon:false}) {
     nodeType: "nginx-dockerized",
     tag: "1.16.0",
     count: 1,
-    flexibleCloudlets: ${settings.bl_flexibleCloudlets:8},
+    flexibleCloudlets: ${settings.bl_flexibleCloudlets:1},
     fixedCloudlets: ${settings.bl_fixedCloudlets:1},
-    diskLimit: ${settings.bl_diskLimit:10},
+    diskLimit: ${settings.bl_diskLimit:5},
     nodeGroup: "bl",
     scalingMode: "STATEFUL",
-    displayName: "Load balancer"
+    displayName: "DHC Load Balancer"
   }, {
     nodeType: "nginxphp-dockerized",
     tag: "1.16.0-php-7.3.8",
-    count: ${settings.cp_count:2},
-    flexibleCloudlets: ${settings.cp_flexibleCloudlets:8},                  
+    count: ${settings.cp_count:1},
+    flexibleCloudlets: ${settings.cp_flexibleCloudlets:1},                  
     fixedCloudlets: ${settings.cp_fixedCloudlets:1},
-    diskLimit: ${settings.cp_diskLimit:10},
+    diskLimit: ${settings.cp_diskLimit:5},
     nodeGroup: "cp",
     scalingMode: "STATELESS",
-    displayName: "AppServer",
+    displayName: "DHC WordPress",
     env: {
-      SERVER_WEBROOT: "/var/www/webroot/ROOT",
+      SERVER_WEBROOT: "/mnt/www/default",
       REDIS_ENABLED: "true"
     },
     volumes: [
-      "/var/www/webroot/ROOT",
-      "/var/www/webroot/.cache",
+      "/mnt/www/default",
+      "/mnt/www/.cache",
       "/etc/nginx/conf.d/SITES_ENABLED"
     ],  
     volumeMounts: {
-      "/var/www/webroot/ROOT": {
+      "/mnt/www/default": {
         readOnly: "false",
         sourcePath: "/data/ROOT",
         sourceNodeGroup: "storage"
       },
-      "/var/www/webroot/.cache": {
+      "/mnt/www/.cache": {
         readOnly: "false",
         sourcePath: "/data/.cache",
         sourceNodeGroup: "storage"
